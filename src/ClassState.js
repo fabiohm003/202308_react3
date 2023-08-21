627062ac2ee43d398e0801b0347cdb58a43ee6a6
Fabio H. Mancipe <fabiohm003@yahoo.es>
2023-08-21 12:42:30 -0500

    1

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 
+const SECURITY_CODE = 'paradigma';
+
 
 export class ClassState extends React.Component {
 
@@ -9,13 +11,69 @@ export class ClassState extends React.Component {
     super(props);
 
     this.state = {
+      value: '',
       error: false,
+      loading: false,
     };
   }
+  
+
+
+  UNSAFE_componentWillMount(){
+    console.log("componentWillMount");
+  }
+  /*montar componente: 1er metodo que se ejecuta cuando se usa React.Component, y cuando se hace render por 1vez*/
+
+
+  componentDidMount(){
+    console.log("componentDidMount");
+  }
+  /*Componente montado: ultimpo metodo que se ejecuta cuando se usa React.Component, y cuando se hace render por 1vez*/
+
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+
+    if (!!this.state.loading){
+
+      setTimeout(() => {
+        console.log("componentDidUpdate: Realizando validación");
+  
+
+
+        if (this.state.value === SECURITY_CODE){
+          console.log("Igualdad");
+          this.setState( { error: false, loading: false } );
+        } else {
+          this.setState( { error: true, loading: false } );
+        }
+
+
+
+  
+        console.log("componentDidUpdate: Terminando validación");
+      }, 3000);
+ 
+    }
+
+  }
+  /*Componente actualizado: se ejecuta cuando se identifica alguna actualizacion en el render*/
+
+
+  componentWillUnmount(){
+    console.log("componentWillUnmount");
+  }
+  /*se aplica cuando se des-renderiza el componente; un ejemplo puede ser cuando se tiene una condicion en donde se desea mostrar/ocultar un objeto por pantalla; al ocultar el objeto, se des-renderiza; al mostrar, se renderiza*/
+
+
+
 
 
 
   render (){
+
+
+    console.log('value:', this.state.value);
 
     return (
       <div>
@@ -28,12 +86,25 @@ export class ClassState extends React.Component {
         )}
 
 
-        <input placeholder='Código de seguridad' />
+
+        {this.state.loading && (
+          <p>Cargando....</p>
+        )}
+
+        <input 
+          placeholder='Código de seguridad'
+          value={this.state.value}
+          onChange={(event) => {
+            this.setState( { value: event.target.value } );
+          }}
+        />
+
+
         <button
-        onClick={()=>(
-          this.setState( {error: !this.state.error} )
-          )}
-        /*prevState*/
+
+        onClick={()=>{
+          this.setState( { error: false, loading: true } );
+        }}
         
         >Comprobar</button>
       </div>
--- src/useState.js
+++ src/useState.js
@@ -1,29 +1,178 @@
 import React from 'react';
 
 
+
+const SECURITY_CODE = 'paradigma';
+
+
 export function UseState ({name}){
 
-  const [error, setError] = React.useState(false);
+
+  const [state, setState] = React.useState({
+    value: '',
+    error: false,
+    loading: false,
+    deleted: false,
+    confirmed: false,
+  });
+
+
+  const onConfirm = () => {
+    setState({
+      ...state, 
+      error: false, 
+      loading: false, 
+      confirmed: true
+    });
+  };
+
+  const onError = () => {
+    setState({
+      ...state, 
+      error: true, 
+      loading: false
+    });
+  };
+
+
+  const onWrite = (valor) => {
+    setState({
+      ...state, 
+      value: valor,
+    });
+  };
+
+  const onCheck = () => {
+    setState({
+      ...state, 
+      error: false, 
+      loading: true
+    });
+  }; 
+
+
+  const onDelete = () => {
+    setState({
+      ...state,
+      deleted: true,
+    });
+  };
+
+
+  const onReset = () => {
+    setState({
+      ...state,
+      confirmed: false,
+      deleted: false,
+      value: '',
+      });
+  };
 
 
 
-  return (
-    <div>
-      <h2>eliminar {name}</h2>
-      <p>Escribe el código de seguridad</p>
 
-      {error && (
-        <p>el código es incorrecto</p>
-      )}
+  console.log('value:', state.value);
 
 
-      <input placeholder='Código de seguridad' />
+  React.useEffect(() => {
 
-      <button
-      onClick={()=>setError(!error)}
-      >Comprobar</button>
-    </div>
-  );
+    console.log("empezar efecto");
+
+    if (!!state.loading){
+
+      setTimeout(() => {
+
+        console.log("Realizando validación");
+
+  
+        if (state.value === SECURITY_CODE){
+          onConfirm();
+          console.log("Igualdad");
+        } else {
+          onError();
+        }
+
+        
+        console.log("Terminando validación");
+      }, 3000);
+ 
+    }
+
+
+    console.log("terminar efecto");
+
+  }, [state.loading]);
+
+
+
+
+
+  if (!state.deleted && !state.confirmed){
+
+    return (
+      <div>
+        <h2>eliminar {name}</h2>
+        <p>Escribe el código de seguridad</p>
+  
+        {state.error && (
+          <p>el código es incorrecto</p>
+        )}
+  
+        {state.loading && (
+          <p>Cargando...</p>
+        )}
+  
+        <input 
+          placeholder='Código de seguridad (useState)'
+          value={state.value} 
+
+          onChange={(event)=>{
+            onWrite(event.target.value);
+          }}
+        />
+  
+  
+        <button
+        onClick={()=>{
+          onCheck();
+        }}
+        >Comprobar</button>
+      </div>
+    );
+
+  } else if (!!state.confirmed && !state.deleted) {
+    return (
+      <>
+        <p>Solicitud de confirmación</p>
+        <button
+        onClick={() => {
+          onDelete();
+        }} 
+
+        >si - eliminar</button>
+        <button
+          onClick={() => {
+            onReset();
+          }} 
+        >no - cancelar</button>
+      </>
+    );
+
+  } else {
+
+    return (
+      <>
+        <p>Eliminado con éxito</p>
+        <button
+        onClick={() => {
+          onReset();
+        }}
+        >Regresar</button>
+      </>
+    );
+
+  }
+
 
 }
 
